7d2ddcb1a3da904989b1741b90a713ce468df2c1
xiebing <xiebing@fairlandgroup.com>
2026-02-11 19:32:58 +0800

    Refactor MapEditorController: rename mapPoints to mapMarkers, update edit mode and related methods

--- src/components/mapeditorcontroller.cc
+++ src/components/mapeditorcontroller.cc
@@ -5,10 +5,12 @@
 
 MapEditorController::MapEditorController(QObject* parent) : QObject(parent) {}
 
+//! 获取编辑模式
 MapEditorController::EditMode MapEditorController::editMode() const {
     return _edit_mode;
 }
 
+//! 设置编辑模式
 void MapEditorController::setEditMode(EditMode mode) {
     if (_edit_mode != mode) {
         clearAllSelectedForMode(_edit_mode);
@@ -17,14 +19,29 @@ void MapEditorController::setEditMode(EditMode mode) {
     }
 }
 
-QVariantList MapEditorController::mapPoints() const {
+//! 获取编辑锁状态
+bool MapEditorController::locked() const {
+    return _locked;
+}
+
+//! 设置编辑锁状态
+void MapEditorController::setLocked(bool locked) {
+    if (_locked != locked) {
+        _locked = locked;
+        emit lockedChanged();
+    }
+}
+
+//! 获取所有标记点
+QVariantList MapEditorController::mapMarkers() const {
     QVariantList list;
-    for (const auto& point : _map_points) {
+    for (const auto& point : _map_markers) {
         list.append(QVariant::fromValue(point));
     }
     return list;
 }
 
+//! 获取所有多边形
 QVariantList MapEditorController::mapPolygons() const {
     QVariantList list;
     for (const auto& polygon : _map_polygons) {
@@ -32,7 +49,6 @@ QVariantList MapEditorController::mapPolygons() const {
     }
     return list;
 }
-
 // QVariantList MapEditorController::mapLineStrings() const {
 //     QVariantList list;
 //     for (const auto& line : _map_linestrings) {
@@ -44,54 +60,50 @@ QVariantList MapEditorController::mapPolygons() const {
 //     return list;
 // }
 
-void MapEditorController::append(QGeoCoordinate coordinate) {
-    switch (_edit_mode) {
-        case EditMode::POINT: {
-            clearAllSelectedForMode(EditMode::POINT);
-            MapPoint* point = new MapPoint(coordinate, this);
-            _map_points.append(point);
-            emit mapPointsChanged();
-            break;
-        }
-        case EditMode::LINESTRING:
-            break;
-        case EditMode::POLYGON:
-            if (_map_polygons.isEmpty()) {
-                MapPolygon* polygon = new MapPolygon(this);
-                polygon->append(coordinate);
-                _map_polygons.append(polygon);
-                emit mapPolygonsChanged();
-            } else {
-                _map_polygons.last()->append(coordinate);
-            }
-            break;
-        default:
-            break;
-    }
-    emit canCloseChanged();
-}
-
+//! 删除所有数据
 void MapEditorController::clearAll() {
-    for (MapPoint* point : _map_points) {
+    for (MapPoint* point : _map_markers) {
         point->deleteLater();
     }
-    _map_points.clear();
-    emit mapPointsChanged();
+    _map_markers.clear();
+    emit mapMarkersChanged();
+
+    for (MapPolygon* polygon : _map_polygons) {
+        polygon->deleteLater();
+    }
+    _map_polygons.clear();
+    emit mapPolygonsChanged();
 }
 
+//! 删除选中数据
 void MapEditorController::deleteSelected() {
     switch (_edit_mode) {
-        case EditMode::POINT: {
-            auto it = _map_points.begin();
-            while (it != _map_points.end()) {
+        case EditMode::MARKER: {
+            auto it = _map_markers.begin();
+            while (it != _map_markers.end()) {
                 if ((*it)->selected()) {
                     (*it)->deleteLater();
-                    it = _map_points.erase(it);
+                    it = _map_markers.erase(it);
                 } else {
                     ++it;
                 }
             }
-            emit mapPointsChanged();
+            emit mapMarkersChanged();
+            break;
+        }
+        case EditMode::LINESTRING:
+            break;
+        case EditMode::POLYGON: {
+            auto it = _map_polygons.begin();
+            while (it != _map_polygons.end()) {
+                if ((*it)->selected()) {
+                    (*it)->deleteLater();
+                    it = _map_polygons.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+            emit mapPolygonsChanged();
             break;
         }
         default:
@@ -99,29 +111,13 @@ void MapEditorController::deleteSelected() {
     }
 }
 
-void MapEditorController::closeCurrentGeometry() {
-    // todo: 实现自动闭合逻辑
-}
-
-bool MapEditorController::locked() const {
-    return _locked;
-}
-
-void MapEditorController::setLocked(bool locked) {
-    if (_locked != locked) {
-        _locked = locked;
-        emit lockedChanged();
-    }
-}
-
-bool MapEditorController::canClose() const {
-    bool can_close = false;
-    switch (_edit_mode) {
-        case EditMode::POINT:
-            for (MapPoint* point : _map_points) {
-                if (point->selected() && point->canClose() && !point->closed()) {
-                    can_close = true;
-                    break;
+//! 清除当前编辑模式下所有选中状态
+void MapEditorController::clearAllSelectedForMode(EditMode mode) {
+    switch (mode) {
+        case EditMode::MARKER:
+            for (MapPoint* point : _map_markers) {
+                if (point->selected()) {
+                    point->setSelected(false);
                 }
             }
             break;
@@ -129,25 +125,7 @@ bool MapEditorController::canClose() const {
             break;
         case EditMode::POLYGON:
             for (MapPolygon* polygon : _map_polygons) {
-                if (polygon->selected() && polygon->canClose() && !polygon->closed()) {
-                    can_close = true;
-                    break;
-                }
-            }
-            break;
-        default:
-            break;
-    }
-    return can_close;
-}
-
-void MapEditorController::clearAllSelectedForMode(EditMode mode) {
-    switch (mode) {
-        case EditMode::POINT:
-            for (MapPoint* point : _map_points) {
-                if (point->selected()) {
-                    point->setSelected(false);
-                }
+                polygon->setSelected(false);
             }
             break;
         default:
@@ -155,27 +133,114 @@ void MapEditorController::clearAllSelectedForMode(EditMode mode) {
     }
 }
 
+//! 清除所有选中状态
 void MapEditorController::clearAllSelected() {
-    clearAllSelectedForMode(EditMode::POINT);
+    clearAllSelectedForMode(EditMode::MARKER);
     clearAllSelectedForMode(EditMode::LINESTRING);
     clearAllSelectedForMode(EditMode::POLYGON);
 }
 
-void MapEditorController::setSelectedItemAndClearOthers(int item_id) {
+//! 设置选中项，并清除当前编辑模式下其他项的选中状态
+void MapEditorController::setSelectedItemAndClearOthers(const QString& uuid) {
     clearAllSelected();
-    for (const auto& point : _map_points) {
-        if (point->id() == item_id) {
+    switch (_edit_mode) {
+        case EditMode::MARKER: {
+            for (const auto& point : _map_markers) {
+                if (point->uuid() == uuid) {
+                    point->setSelected(true);
+                    return;
+                }
+            }
+            break;
+        }
+        case EditMode::LINESTRING:
+            break;
+        case EditMode::POLYGON: {
+            for (const auto& polygon : _map_polygons) {
+                if (polygon->uuid() == uuid) {
+                    polygon->setSelected(true);
+                    return;
+                }
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+//! 设置选中项
+void MapEditorController::setSelectedItem(const QString& uuid) {
+    for (const auto& point : _map_markers) {
+        if (point->uuid() == uuid) {
             point->setSelected(true);
             return;
         }
     }
 }
 
-void MapEditorController::setSelectedItem(int item_id) {
-    for (const auto& point : _map_points) {
-        if (point->id() == item_id) {
-            point->setSelected(true);
-            return;
+//! 添加操作
+void MapEditorController::append(QGeoCoordinate coordinate) {
+    switch (_edit_mode) {
+        case EditMode::MARKER: {
+            clearAllSelectedForMode(EditMode::MARKER);
+            MapPoint* point = new MapPoint(coordinate, this);
+            _map_markers.append(point);
+            emit mapMarkersChanged();
+            break;
         }
+        case EditMode::LINESTRING:
+            break;
+        case EditMode::POLYGON: {
+            bool appended = false;
+            for (auto& polygon : _map_polygons) {
+                if (polygon->selected()) {
+                    polygon->append(coordinate);
+                    appended = true;
+                    emit mapPolygonsChanged();
+                    break;
+                }
+            }
+            if (!appended) {
+                clearAllSelected();
+                MapPolygon* polygon = new MapPolygon(this);
+                polygon->append(coordinate);
+                _map_polygons.append(polygon);
+                emit mapPolygonsChanged();
+            }
+            break;
+        }
+        default:
+            break;
     }
+}
+
+//! 结束当前当前编辑对象的操作
+void MapEditorController::finishCurrentEditing() {
+    switch (_edit_mode) {
+        case EditMode::MARKER:
+            break;
+        case EditMode::LINESTRING:
+            break;
+        case EditMode::POLYGON: {
+            //! 如果多边形不是有效的则删除未闭合的多边形，如果当前编辑的多边形未闭合且是有效的，则进行闭合
+            auto it = _map_polygons.begin();
+            while (it != _map_polygons.end()) {
+                if (!(*it)->valid()) {
+                    (*it)->deleteLater();
+                    it = _map_polygons.erase(it);
+                } else {
+                    if ((*it)->selected() && !(*it)->closed() && (*it)->valid()) {
+                        (*it)->closeGeometry();
+                    }
+                    ++it;
+                }
+            }
+            emit mapPolygonsChanged();
+            break;
+        }
+        default:
+            break;
+    }
+    clearAllSelected();
 }
--- src/components/mapeditorcontroller.h
+++ src/components/mapeditorcontroller.h
@@ -18,37 +18,38 @@ class MapEditorController : public QObject {
 
     Q_PROPERTY(EditMode editMode READ editMode WRITE setEditMode NOTIFY editModeChanged)
     Q_PROPERTY(bool locked READ locked WRITE setLocked NOTIFY lockedChanged)
-    Q_PROPERTY(bool canClose READ canClose NOTIFY canCloseChanged)
 
-    Q_PROPERTY(QVariantList mapPoints READ mapPoints NOTIFY mapPointsChanged)
+    Q_PROPERTY(QVariantList mapMarkers READ mapMarkers NOTIFY mapMarkersChanged)
     Q_PROPERTY(QVariantList mapPolygons READ mapPolygons NOTIFY mapPolygonsChanged)
     // Q_PROPERTY(QVariantList mapLineStrings READ mapLineStrings NOTIFY mapLineStringsChanged)
 
 public:
-    enum EditMode { POINT = 0, LINESTRING, POLYGON, CIRCLE, SQUARE }; //! 编辑模式
+    enum EditMode { MARKER = 0, LINESTRING, POLYGON, CIRCLE, SQUARE }; //! 编辑模式
     Q_ENUM(EditMode)
 
     explicit MapEditorController(QObject* parent = nullptr);
 
-    EditMode editMode() const; //! 获取当前编辑模式
-    void setEditMode(EditMode mode); //! 设置当前编辑模式
+    //! 编辑模式
+    EditMode editMode() const;
+    void setEditMode(EditMode mode);
 
-    QVariantList mapPoints() const; //! 获取所有标记点
+    //! 编辑锁定
+    bool locked() const;
+    void setLocked(bool locked);
+
+    QVariantList mapMarkers() const; //! 获取所有标记点
     QVariantList mapPolygons() const; //! 获取所有多边形
-
     // QVariantList mapLineStrings() const; //! 获取所有线
-
-    bool locked() const; //! 获取编辑锁状态
-    void setLocked(bool locked); //! 设置编辑锁状态
-    bool canClose() const; //! 获取当前编辑对象是否可以闭合
 
     Q_INVOKABLE void append(QGeoCoordinate coordinate); //! 添加
     Q_INVOKABLE void clearAll(); //! 删除所有数据
     Q_INVOKABLE void deleteSelected(); //! 删除选中数据
-    Q_INVOKABLE void closeCurrentGeometry(); //! 闭合当前编辑对象
-    Q_INVOKABLE void setSelectedItemAndClearOthers(int item_id); //! 设置选中项，并清除其他项的选中状态
-    Q_INVOKABLE void setSelectedItem(int item_id); //! 设置选中项，不清除其他项的选中状态
+    Q_INVOKABLE void setSelectedItemAndClearOthers(const QString& uuid); //! 设置选中项，并清除其他项的选中状态
+    Q_INVOKABLE void setSelectedItem(const QString& uuid); //! 设置选中项，不清除其他项的选中状态
     Q_INVOKABLE void clearAllSelected(); //! 清除所有选中状态
+
+    //! 结束当前当前编辑对象的操作
+    Q_INVOKABLE void finishCurrentEditing();
 
 private:
     void clearAllSelectedForMode(EditMode mode); //! 清除当前编辑模式下所有选中状态
@@ -56,17 +57,16 @@ private:
 signals:
     void editModeChanged(); //! 编辑模式改变信号
     void lockedChanged(); //! 编辑锁状态改变信号
-    void canCloseChanged(); //! 是否可以闭合改变信号
 
-    void mapPointsChanged(); //! 标记点列表改变信号
+    void mapMarkersChanged(); //! 标记点列表改变信号
     void mapLineStringsChanged(); //! 线列表改变信号
     void mapPolygonsChanged(); //! 多边形列表改变信号
 
 private:
-    EditMode _edit_mode{EditMode::POINT}; //! 当前编辑模式
+    EditMode _edit_mode{EditMode::MARKER}; //! 当前编辑模式
     bool _locked{false}; //! 编辑锁，防止在编辑过程中进行其他操作
 
-    QList<MapPoint*> _map_points{}; //! MapPoint列表
+    QList<MapPoint*> _map_markers{}; //! MapMarker列表
     QList<MapLineString*> _map_linestrings{}; //! MapLineString列表
     QList<MapPolygon*> _map_polygons{}; //! MapPolygon列表
 };
